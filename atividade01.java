cd8374b3fc73bb5cba2e980633b6267f1e62afe6
Henrique <htrentin17@gmail.com>
2024-09-01 19:11:02 -0300

    .

---
+++ atividade01.java
@@ -0,0 +1,15 @@
+public class atividade01 {
+    public static void main(String[] args) {
+        //Crie uma variável que armazene a idade de uma pessoa
+        int idade = 18;
+
+        //Verifique se a pessoa é maior de idade (18 anos ou mais)
+        if (idade >= 18) {
+
+            //Imprima uma mensagem informando se a pessoa é maior de idade ou menor de idade
+            System.out.println("A pessoa é maior de idade.");
+        } else {
+            System.out.println("A pessoa é menor de idade.");
+        }
+    }
+}
